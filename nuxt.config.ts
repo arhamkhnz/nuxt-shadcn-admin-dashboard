f2f4bcacd22991b5360fa087a4286922f5264012
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2025-08-20 00:27:15 +0530

    chore: reorganized components under app/components for consistency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,15 +10,8 @@ export default defineNuxtConfig({
   },
   modules: ["@nuxt/eslint", "@nuxt/fonts", "shadcn-nuxt"],
   shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
     prefix: "",
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
-    componentDir: "./components/ui",
+    componentDir: "./app/components/ui",
   },
   routeRules: {
     "/": { redirect: "/dashboard/default" },
